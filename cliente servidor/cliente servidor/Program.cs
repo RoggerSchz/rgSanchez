adc4b67f1079b02cc23f9447b9232131d253999c
Rogelio <dickiess.93@hotmail.com>
2014-09-20 00:52:57 -0500

    Cliente servidor Signed-off-by: Rogelio <dickiess.93@hotmail.com>

---
+++ cliente servidor/cliente servidor/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace cliente_servidor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conectar();
+
+        }
+        public static void Conectar()
+        { 
+          Socket miPrimerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+          IPEndPoint miDireccion = new IPEndPoint(IPAddress.Any, 1234);
+          try
+          {
+              miPrimerSocket.Bind(miDireccion);
+              miPrimerSocket.Listen(1);
+              Console.Write("Escuchando");
+              Socket Escuchar = miPrimerSocket.Accept();
+              Console.Write("Cliente conectado");
+
+              Console.WriteLine("Connecion aceptada" + Escuchar.RemoteEndPoint); 
+              byte[] b = new byte[100];
+              int k = Escuchar.Receive(b);
+              Console.WriteLine("Recibiendo...");
+              for (int i = 0; i < k; i++)
+                  Console.Write(Convert.ToChar(b[i]));
+
+              ASCIIEncoding asen = new ASCIIEncoding();
+              Escuchar.Send(asen.GetBytes("Cadena recibida desde el cliente"));
+             
+              miPrimerSocket.Close();
+
+          }
+          catch(Exception error) 
+          {
+              Console.WriteLine("Error:", error.ToString());
+
+          }
+          Console.WriteLine("Pesione  una tecla para termianr");
+          Console.ReadLine();
+        }
+    }
+}
---
+++ cliente/cliente/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;           
+using System.Net.Sockets;
+using System.IO;
+namespace cliente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conectar();
+
+        }
+        public static void Conectar()
+        {
+         try 
+         { 
+            TcpClient tcpclnt = new TcpClient(); 
+            Console.WriteLine("Connectando...");
+
+            tcpclnt.Connect("127.0.0.1", 1234); 
+
+            Console.WriteLine("Conectado"); 
+            Console.Write("Escriba el mensaje a enviar : "); 
+
+            String str = Console.ReadLine(); 
+            Stream stm = tcpclnt.GetStream(); 
+
+            ASCIIEncoding asen = new ASCIIEncoding(); 
+            byte[] ba = asen.GetBytes(str); 
+
+            stm.Write(ba, 0, ba.Length); 
+
+            byte[] bb = new byte[100]; 
+            int k = stm.Read(bb, 0, 100); 
+
+            for (int i = 0; i < k; i++) 
+            Console.Write(Convert.ToChar(bb[i])); 
+
+            tcpclnt.Close(); 
+          } 
+
+          catch (Exception e) 
+          { 
+                Console.WriteLine("Error..... " + e.StackTrace); 
+          } 
+
+          Console.ReadKey(); 
+            } 
+       }   
+ }
+
